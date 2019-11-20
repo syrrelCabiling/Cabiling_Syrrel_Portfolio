e932582f54e8c039100a5e2b05bcefd3790104a1
Syrrel Cabiling <syrrelcabiling@Syrrels-MacBook-Pro.local>
2019-11-20 00:41:40 -0500

    splash semi working

--- public/js/burger-menu.js
+++ public/js/burger-menu.js
@@ -1,15 +1,15 @@
 //expanding and hiding the burger menu when clicked
 
 const hideExpand = () => {
-    const burger = document.querySelector('.burger-menu');
-    const nav = document.querySelector('.navlinks');
+    const burger = document.querySelector('.burger-menu'); //class from _nav; these are the lines
+    const nav = document.querySelector('.navlinks'); //links
     const eachLink = document.querySelectorAll('.navlinks li');
 
-    burger.addEventListener('click', ()=>{
+    burger.addEventListener('click', ()=>{ //toggle function
         nav.classList.toggle('burger-expand');
 
          eachLink.forEach((link, index) => {
-             link.style.animation = `LinksFading 0.2s ease forwards ${index / 8 + 0.5}s`;
+             link.style.animation = `LinksFading 0.2s ease forwards ${index / 8 + 0.5}s`; //fading in each link
         });
 
         burger.classList.toggle('toggleBurger');
