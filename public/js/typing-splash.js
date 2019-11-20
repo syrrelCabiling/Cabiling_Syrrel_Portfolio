3bd258e200233ac6e70fd70e1f2c51b1d604bec1
Syrrel Cabiling <syrrelcabiling@Syrrels-MacBook-Pro.local>
2019-11-20 12:04:22 -0500

    typewriter on splash is working! yay

---
+++ public/js/typing-splash.js
@@ -0,0 +1,37 @@
+// script for the typing texts on splash screen
+
+var eachLetter = 0;
+var adding = 0;
+var passion = ['user experience','user interface','product design','graphic design'];
+var speed = 100;
+
+setTimeout(typingTexts, speed);
+
+//typing 
+function typingTexts() {
+    if(eachLetter < passion[adding].length){
+        document.getElementById("demo").innerHTML += passion[adding].charAt(eachLetter);
+        eachLetter++;
+        setTimeout(typingTexts, speed);
+    }else{
+        setTimeout(erase, speed+10);
+    }
+}
+
+//deleting each letter then retyping again
+function erase() {
+	if (eachLetter >= 0) {
+      var str=passion[adding].toString().substring(0, eachLetter);
+      document.getElementById("demo").innerHTML = str;
+      eachLetter--;
+      setTimeout(erase, speed-25);
+    } else {
+        adding++;
+      if(adding>=passion.length) 
+        adding=0;
+      setTimeout(typingTexts, speed);
+    }
+}
+
+
+
