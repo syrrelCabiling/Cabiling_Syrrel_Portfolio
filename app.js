6f6266d3366b7be1288db9082ba69c1e651ce047
Syrrel Cabiling <syrrelcabiling@Syrrels-MacBook-Pro.local>
2019-12-03 23:45:24 -0500

    added contact html; will change later this wont work

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', require('./routes/index'));
+
+
 app.use((req, res, next) => {
     var err = new Error('Not Found');
     err.status = 404;
