67125909ca242db9ff671022f6e29e5eeaa82fda
Syrrel Cabiling <syrrelcabiling@Syrrels-MacBook-Pro.local>
2019-12-08 15:08:48 -0500

    changed port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     host: "us-cdbr-iron-east-05.cleardb.net",
-    port: 8889, // 3306 for windows
+    //port: 8889, // 3306 for windows
     uname: "bfc4a62efc32ae",
     pword: "17c047a6", // leave blank for windows
     database: "heroku_0341ca2533ba259"
--- utils/sql.js
+++ utils/sql.js
@@ -3,13 +3,13 @@ const sql = require('mysql');
 
 const connect = sql.createConnection({
     host: config.host,
-   //port: config.port,
-  user: config.user,
-  password: config.password,
-  database: config.database,
-  connectionLimit : 20,
-  queueLimit : 100,
-  waitForConnections : true
+   port: config.port,
+    user: config.user,
+    password: config.password,
+    database: config.database,
+    connectionLimit : 20,
+    queueLimit : 100,
+    waitForConnections : true
 });
 
 module.exports = connect;
