c7964b2758713ebf8c6d333cc38bf9e60204bf6a
Syrrel Cabiling <syrrelcabiling@Syrrels-MacBook-Pro.local>
2019-12-08 15:38:09 -0500

    chaged spelling in sql

--- utils/sql.js
+++ utils/sql.js
@@ -4,8 +4,8 @@ const sql = require('mysql');
 const connect = sql.createPool({
     host: config.host,
    //port: config.port,
-    user: config.user,
-    password: config.password,
+    user: config.uname,
+    password: config.pword,
     database: config.database,
     connectionLimit : 20,
     queueLimit : 100,
